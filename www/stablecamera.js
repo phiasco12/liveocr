6dc6a6b187afa5377e29f9f1e8afc3eecac54125
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2025-11-12 17:59:32 +0000

    Update stablecamera.js

--- www/stablecamera.js
+++ www/stablecamera.js
@@ -1,6 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.start = function(success, error, options) {
-  exec(success, error, 'StableCamera', 'start', [options]);
+exports.start = function (success, error) {
+  exec(success, error, 'StableCamera', 'start', []);
 };
-
